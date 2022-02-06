304c8597641ac4d9a05d0a76416a3a9d0af806fb
TaiseiIto <taisei1212@outlook.jp>
2022-02-06 16:46:18 +0900

    第二章

---
+++ 2/functions.hs
@@ -0,0 +1,9 @@
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
