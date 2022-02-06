3d3b959b655ef09de5eaf9984c36a5687e8be077
TaiseiIto <taisei1212@outlook.jp>
2022-02-06 15:47:32 +0900

    型を書いてみた

--- 1/baby.hs
+++ 1/baby.hs
@@ -1,16 +1,24 @@
+doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
+doubleUs :: Num a => a -> a -> a
 doubleUs x y = doubleMe x + doubleMe y
 
+doubleSmallNumber :: (Num a, Ord a) => a -> a
 doubleSmallNumber x = if x > 100 then x else x * 2
 
+doubleSmallNumber' :: (Num a, Ord a) => a -> a
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 
+conanO'Brien :: String
 conanO'Brien = "It's a me, Conan O'Brien!"
 
+boomBangs :: Integral a => [a] -> [String]
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
+length' :: Num b => [a] -> b
 length' xs = sum [1 | _ <- xs]
 
+removeNonUppercase :: String -> String
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 
