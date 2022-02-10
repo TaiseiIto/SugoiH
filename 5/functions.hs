7ebb2c2b9dc0d12371bf08323a49f8c9f53babf9
TaiseiIto <taisei1212@outlook.jp>
2022-02-10 18:02:16 +0900

    第五章

---
+++ 5/functions.hs
@@ -0,0 +1,40 @@
+{-# OPTIONS -Wall -Werror #-}
+
+mult3 :: (Num a) => a -> a -> a -> a
+mult3 x y z = x * y * z
+
+compareWith100 :: Int -> Ordering
+compareWith100 = compare 100
+
+divideBy10 :: (Floating a) => a -> a
+divideBy10 = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _          = []
+zipWith' _ _ []          = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+ | f x       = x : filter' f xs
+ | otherwise = filter' f xs
+
