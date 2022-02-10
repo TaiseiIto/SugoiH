d742034d43b93476306441e34ee30eb51b34c871
TaiseiIto <taisei1212@outlook.jp>
2022-02-10 21:23:01 +0900

    第五章

--- 5/functions.hs
+++ 5/functions.hs
@@ -28,13 +28,53 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f y x = f x y
 
-map' :: (a -> b) -> [a] -> [b]
-map' _ [] = []
-map' f (x:xs) = f x : map' f xs
+map0 :: (a -> b) -> [a] -> [b]
+map0 _ [] = []
+map0 f (x:xs) = f x : map0 f xs
 
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' _ [] = []
-filter' f (x:xs)
- | f x       = x : filter' f xs
- | otherwise = filter' f xs
+map1 :: (a -> b) -> [a] -> [b]
+map1 f = foldr (\x xs -> f x : xs) []
+
+map2 :: (a -> b) -> [a] -> [b]
+map2 f = foldl (\xs x -> xs ++ [f x]) []
+
+filter0 :: (a -> Bool) -> [a] -> [a]
+filter0 _ [] = []
+filter0 f (x:xs)
+ | f x       = x : filter0 f xs
+ | otherwise = filter0 f xs
+
+filter1 :: (a -> Bool) -> [a] -> [a]
+filter1 f = foldr (\x xs -> if f x then x:xs else xs) []
+
+collatz :: Int -> [Int]
+collatz n
+ | n <= 0    = []
+ | n == 1    = [1]
+ | even n    = n : collatz (n `div` 2)
+ | otherwise = n : collatz (3 * n + 1)
+
+numLongChains :: Int
+numLongChains = let isLong xs = length xs > 15 in length (filter isLong (map collatz [1..100]))
+
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map collatz [1..100]))
+
+sum' :: (Foldable f, Num n) => f n -> n
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' x = foldr (\a b -> if a == x then True else b) False
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+last' :: [a] -> a
+last' = foldl1 (\_ b -> b)
 
