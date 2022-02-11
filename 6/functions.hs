564af262fc3204501de21c5ecf87fc6646e16a4d
TaiseiIto <taisei1212@outlook.jp>
2022-02-11 22:05:11 +0900

    第六章

--- 6/functions.hs
+++ 6/functions.hs
@@ -4,13 +4,13 @@ import qualified Data.Char
 import qualified Data.List
 import qualified Data.Map
 
-numUniques :: (Eq a) => [a] -> Int
+numUniques :: Eq a => [a] -> Int
 numUniques = length . Data.List.nub
 
 wordNums :: String -> [(String, Int)]
 wordNums = map (\wordList -> (head wordList, length wordList)) . Data.List.group . Data.List.sort . Data.List.words
 
-isInfixOf :: (Eq a) => [a] -> [a] -> Bool
+isInfixOf :: Eq a => [a] -> [a] -> Bool
 needle `isInfixOf` haystack = any (needle `Data.List.isPrefixOf`) $ Data.List.tails haystack
 
 encode :: Int -> String -> String
@@ -25,8 +25,8 @@ digitSum = sum . map Data.Char.digitToInt . show
 firstTo :: Int -> Maybe Int
 firstTo n = Data.List.find ((n==) . digitSum) [1..]
 
-phoneBook :: Data.Map.Map String String
-phoneBook = Data.Map.fromList
+phoneBook :: [(String,String)]
+phoneBook = 
  [
   ("betty","555-2938"),
   ("betty","342-2492"),
@@ -36,13 +36,16 @@ phoneBook = Data.Map.fromList
   ("patsy","827-9162"),
   ("lucille","205-2928"),
   ("wendy","939-8282"),
-  ("penny","853-2492")
+  ("penny","853-2492"),
   ("penny","555-2111")
  ]
 
-findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey :: Eq k => k -> [(k, v)] -> Maybe v
 findKey key = foldr (\(k,v) a -> if k == key then Just v else a) Nothing
 
 string2digits :: String -> [Int]
 string2digits = map Data.Char.digitToInt . filter Data.Char.isDigit
+
+phoneBookToMap :: Ord k => [(k,String)] -> Data.Map.Map k [String]
+phoneBookToMap = Data.Map.fromListWith (++) . map (\(key,string) -> (key,[string]))
 
