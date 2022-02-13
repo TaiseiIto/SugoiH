5026244039b9bb372525e5888da5051b3b7c5886
TaiseiIto <taisei1212@outlook.jp>
2022-02-13 19:18:46 +0900

    第七章

--- 7/Person.hs
+++ 7/Person.hs
@@ -1,27 +1,11 @@
 data Person = Person
- String -- First name
- String -- Last name
- Int    -- Age
- Float  -- Height
- String -- Phone number
- String -- Favorite ice cream flavor
+ {
+  firstName   :: String,
+  lastName    :: String,
+  age         :: Int,
+  height      :: Float,
+  phoneNumber :: String,
+  flavor      :: String
+ }
  deriving (Show)
-
-firstName :: Person -> String
-firstName (Person _firstName _ _ _ _ _) = _firstName
-
-lastName :: Person -> String
-lastName (Person _ _lastName _ _ _ _) = _lastName
-
-age :: Person -> Int
-age (Person _ _ _age _ _ _) = _age
-
-height :: Person -> Float
-height (Person _ _ _ _height _ _) = _height
-
-phoneNumber :: Person -> String
-phoneNumber (Person _ _ _ _ _phoneNumber _) = _phoneNumber
-
-flavor :: Person -> String
-flavor (Person _ _ _ _ _ _flavor) = _flavor
 
