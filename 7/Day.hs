9d2ff2a2f108d4effdefb0b81bb1c90ed7688bbb
TaiseiIto <taisei1212@outlook.jp>
2022-02-13 20:16:55 +0900

    第七章

---
+++ 7/Day.hs
@@ -0,0 +1,18 @@
+data Day =
+ Monday    |
+ Tuesday   |
+ Wednesday |
+ Thursday  |
+ Friday    |
+ Saturday  |
+ Sunday
+ deriving
+  (
+   Bounded,
+   Enum,
+   Eq,
+   Ord,
+   Read,
+   Show
+  )
+
