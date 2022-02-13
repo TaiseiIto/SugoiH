c292627c6b3fdc4f1e4568466381eb6a617078ce
TaiseiIto <taisei1212@outlook.jp>
2022-02-13 20:07:39 +0900

    第七章

--- 7/Person.hs
+++ 7/Person.hs
@@ -1,11 +1,40 @@
 data Person = Person
  {
-  firstName   :: String,
-  lastName    :: String,
-  age         :: Int,
-  height      :: Float,
-  phoneNumber :: String,
-  flavor      :: String
+  firstName :: String,
+  lastName  :: String,
+  age       :: Int
  }
- deriving (Show)
+ deriving
+ (
+  Eq,
+  Read,
+  Show
+ )
+
+mikeD :: Person
+mikeD = Person
+ {
+  firstName = "Michael",
+  lastName  = "Diamond",
+  age       = 43
+ }
+
+adRock :: Person
+adRock = Person
+ {
+  firstName = "Adam",
+  lastName  = "Horovitz",
+  age       = 41
+ }
+
+mca :: Person
+mca = Person
+ {
+  firstName = "Adam",
+  lastName  = "Yauch",
+  age       = 44
+ }
+
+mysteryDude :: String
+mysteryDude = "Person{firstName = \"Michael\", lastName = \"Diamond\", age = 43}"
 
