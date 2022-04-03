92aeecc343094f4d767685e5fbf4c12b0b816f8e
TaiseiIto <taisei1212@outlook.jp>
2022-04-03 11:11:14 +0900

    再帰的なデータ構造

--- 7/List.hs
+++ 7/List.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS -Wall -Werror #-}
 
-data List a = Empty | Cons a (List a)
+infixr 5 :-:
+data List a = Empty | a :-: (List a)
  deriving
  (
   Show,
@@ -8,4 +9,9 @@ data List a = Empty | Cons a (List a)
   Eq,
   Ord
  )
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ b = b
+(head_a :-: tail_a) ^++ b = head_a :-: (tail_a ^++ b)
 
