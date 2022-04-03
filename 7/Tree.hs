1968431d34129eea5efbb0ed1b978e52d9cf0675
TaiseiIto <taisei1212@outlook.jp>
2022-04-03 12:17:57 +0900

    Tree.hs

---
+++ 7/Tree.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS -Wall -Werror #-}
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) where Node :: Ord a => a -> Tree a -> Tree a -> Tree a
+ deriving (Show)
+
+treeInsert :: Ord a => a -> Tree a -> Tree a
+treeInsert new_value EmptyTree = Node new_value EmptyTree EmptyTree
+treeInsert new_value (None root_value left_subtree right_subtree)
+ | new_value == root_value = Node root_value left_subtree right_subtree
+ | new_value <  root_value = Node root_value (treeInsert new_value left_subtree) right_subtree
+ | new_value >  root_value = Node root_value left_subtree (treeInsert new_value right_subtree)
+
+treeElem :: Ord a => a -> Tree a -> Bool
+treeEmen _ EmptyTree = False
+treeElem value (Node root_value left_subtree right_subtree)
+ | value == root_value = True
+ | value <  root_value = treeElem value left_subtree
+ | value >  root_value = treeElem value right_subtree
+
+numsTree :: Num a => List a -> Tree a
+numbTree = foldr treeInsert EmptyTree
+
