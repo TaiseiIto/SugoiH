7a2eb8bed3ae7606a02cb0c491849caf54056e8d
TaiseiIto <taisei1212@outlook.jp>
2022-04-03 13:27:41 +0900

    Tree.hs

--- 7/Tree.hs
+++ 7/Tree.hs
@@ -12,14 +12,14 @@ treeInsert new_value EmptyTree = Node new_value EmptyTree EmptyTree
 treeInsert new_value (Node root_value left_subtree right_subtree)
  | new_value == root_value = Node root_value left_subtree right_subtree
  | new_value <  root_value = Node root_value (treeInsert new_value left_subtree) right_subtree
- | new_value >  root_value = Node root_value left_subtree (treeInsert new_value right_subtree)
+ | otherwise               = Node root_value left_subtree (treeInsert new_value right_subtree)
 
 treeElem :: Ord a => a -> Tree a -> Bool
 treeElem _ EmptyTree = False
 treeElem value (Node root_value left_subtree right_subtree)
  | value == root_value = True
  | value <  root_value = treeElem value left_subtree
- | value >  root_value = treeElem value right_subtree
+ | otherwise           = treeElem value right_subtree
 
 numsTree :: Ord a => [a] -> Tree a
 numsTree = foldr treeInsert EmptyTree
