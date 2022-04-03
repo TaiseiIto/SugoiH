6ba01e9b1515a06d8e46e82a00f87afce604aaab
TaiseiIto <taisei1212@outlook.jp>
2022-04-03 13:09:36 +0900

    Tree.hs

--- 7/Tree.hs
+++ 7/Tree.hs
@@ -1,11 +1,14 @@
 {-# OPTIONS -Wall -Werror #-}
 
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) where Node :: Ord a => a -> Tree a -> Tree a -> Tree a
+data Tree a
+ where
+  EmptyTree :: Tree a
+  Node :: Ord a => a -> Tree a -> Tree a -> Tree a
  deriving (Show)
 
 treeInsert :: Ord a => a -> Tree a -> Tree a
 treeInsert new_value EmptyTree = Node new_value EmptyTree EmptyTree
-treeInsert new_value (None root_value left_subtree right_subtree)
+treeInsert new_value (Node root_value left_subtree right_subtree)
  | new_value == root_value = Node root_value left_subtree right_subtree
  | new_value <  root_value = Node root_value (treeInsert new_value left_subtree) right_subtree
  | new_value >  root_value = Node root_value left_subtree (treeInsert new_value right_subtree)
@@ -17,6 +20,6 @@ treeElem value (Node root_value left_subtree right_subtree)
  | value <  root_value = treeElem value left_subtree
  | value >  root_value = treeElem value right_subtree
 
-numsTree :: Num a => List a -> Tree a
-numbTree = foldr treeInsert EmptyTree
+numsTree :: Num a => [a] -> Tree a
+numsTree = foldr treeInsert EmptyTree
 
