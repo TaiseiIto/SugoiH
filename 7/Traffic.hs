1866286a06dc0efbc4b6ea6c9e057f4f21f068d3
TaiseiIto <taisei1212@outlook.jp>
2022-05-14 13:21:07 +0900

    Traffic.hs

---
+++ 7/Traffic.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS -Wall -Werror #-}
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight
+ where
+  Red    == Red    = True
+  Green  == Green  = True
+  Yellow == Yellow = True
+  _      == _      = False
+
+instance Show TrafficLight
+ where
+  show Red    = "Red Light"
+  show Green  = "Green Light"
+  show Yellow = "Yellow Light"
+
