2345b3f34bf7fdebd49f210d9d7aa5a9be1a7505
TaiseiIto <taisei1212@outlook.jp>
2022-05-14 19:50:52 +0900

    8.4

--- 8/color.hs
+++ 8/color.hs
@@ -8,8 +8,7 @@ main = do
  System.IO.hSetBuffering System.IO.stdout System.IO.NoBuffering
  colors <- Control.Monad.forM ([1,2,3,4] :: [Int]) $ \a -> do
   putStr $ "Which color do you associate with the number " ++ show a ++ "? : "
-  color <- getLine
-  return color
+  getLine
  putStrLn "The colors that you associate with 1, 2, 3, and 4 are :"
  mapM_ putStrLn colors
  return ()
