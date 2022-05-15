ad0fbf434c9e2f6217d2a8e2974eb22cded7b774
TaiseiIto <taisei1212@outlook.jp>
2022-05-15 10:30:50 +0900

    9.2

---
+++ 9/baabaa.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS -Wall -Werror #-}
+
+import qualified System.IO
+
+main :: IO ()
+main = do
+ file <- System.IO.openFile "baabaa.txt" System.IO.ReadMode
+ contents <- System.IO.hGetContents file
+ putStr contents
+ System.IO.hClose file
+
