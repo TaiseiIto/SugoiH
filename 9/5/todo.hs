e66a934fa2ca33085995baf55ee2fe1e690b29e6
TaiseiIto <taisei1212@outlook.jp>
2022-05-15 14:28:18 +0900

    9.5

--- 9/5/todo.hs
+++ 9/5/todo.hs
@@ -1,6 +1,17 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import qualified System.Environment
+
 main :: IO ()
 main = do
+ commandLineArguments <- System.Environment.getArgs
+ action commandLineArguments
  return ()
+
+action :: [String] -> IO ()
+action [] = do
+ putStrLn "No command"
+ return ()
+action (command : _) = do
+ putStrLn $ "Invalid command : " ++ command
 
