ea18b6775e1da685f30d49b561b88854ac18a64a
TaiseiIto <taisei1212@outlook.jp>
2022-05-18 21:43:17 +0900

    9.1

--- 10/1/reversepolish.hs
+++ 10/1/reversepolish.hs
@@ -1,6 +1,36 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import qualified System.Environment
+
+data Element = 
+ Add |
+ Sub |
+ Mul |
+ Div |
+ Val Float |
+ Nul
+
+instance Read Element where
+ readsPrec _ ('+' : remain) = [(Add, remain)]
+ readsPrec _ ('-' : remain) = [(Sub, remain)]
+ readsPrec _ ('*' : remain) = [(Mul, remain)]
+ readsPrec _ ('/' : remain) = [(Div, remain)]
+ readsPrec _ ('?' : remain) = [(Nul, remain)]
+ readsPrec _ string = case (reads :: String -> [(Float, String)]) string of
+  (val, remain) : _ -> [(Val val, remain)]
+  _                 -> []
+
+instance Show Element where
+ show Add = "+"
+ show Sub = "-"
+ show Mul = "*"
+ show Div = "/"
+ show (Val val) = show val
+ show Nul = "?"
+
 main :: IO ()
 main = do
+ args <- System.Environment.getArgs
+ putStrLn . show . map (read :: String -> Element) $ args
  return ()
 
