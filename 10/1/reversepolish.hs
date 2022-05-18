24179b30de4d8d0504ff8cff7f2d6de1c6766a55
TaiseiIto <taisei1212@outlook.jp>
2022-05-18 22:08:58 +0900

    9.1

--- 10/1/reversepolish.hs
+++ 10/1/reversepolish.hs
@@ -1,5 +1,9 @@
 {-# OPTIONS -Wall -Werror #-}
 
+-- Usage
+-- $ ./reversepolish 1 2 \* 3 4 \* + 5 /
+-- 2.8
+
 import qualified System.Environment
 
 data Element = 
@@ -7,6 +11,7 @@ data Element =
  Sub |
  Mul |
  Div |
+ Nul |
  Val Float
 
 instance Read Element where
@@ -14,6 +19,7 @@ instance Read Element where
  readsPrec _ ('-' : remain) = [(Sub, remain)]
  readsPrec _ ('*' : remain) = [(Mul, remain)]
  readsPrec _ ('/' : remain) = [(Div, remain)]
+ readsPrec _ ('?' : remain) = [(Nul, remain)]
  readsPrec _ string = case (reads :: String -> [(Float, String)]) string of
   (val, remain) : _ -> [(Val val, remain)]
   _                 -> []
@@ -23,11 +29,25 @@ instance Show Element where
  show Sub = "-"
  show Mul = "*"
  show Div = "/"
+ show Nul = "?"
  show (Val val) = show val
 
 main :: IO ()
 main = do
  args <- System.Environment.getArgs
- putStrLn . show . map (read :: String -> Element) $ args
+ putStrLn . show . solve . map (read :: String -> Element) $ args
  return ()
+
+solve :: [Element] -> Element
+solve []                = Nul
+solve [Val val]         = Val val
+solve (element : stack) = solve . step element $ stack
+
+step :: Element -> [Element] -> [Element]
+step Add (Val a : Val b : stack) = Val (a + b) : stack
+step Sub (Val a : Val b : stack) = Val (a - b) : stack
+step Mul (Val a : Val b : stack) = Val (a * b) : stack
+step Div (Val a : Val b : stack) = Val (a / b) : stack
+step (Val val) stack = Val val : stack
+step _ _ = [Nul]
 
