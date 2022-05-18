13b6f8f6ea1a6df5a4ae18e78398d968594e8e18
TaiseiIto <taisei1212@outlook.jp>
2022-05-18 20:34:06 +0900

    9.7

--- 9/7/copy.hs
+++ 9/7/copy.hs
@@ -1,12 +1,35 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import qualified Control.Exception
+import qualified Data.ByteString
+import qualified System.Directory
 import qualified System.Environment
+import qualified System.IO
+
+copy :: String -> String -> IO ()
+copy src dst = do
+ contents <- Data.ByteString.readFile src
+ Control.Exception.bracketOnError
+  (
+   System.IO.openTempFile "." "temp"
+  )
+  (
+   \(tempName, tempFile) -> do
+    System.IO.hClose tempFile
+    System.Directory.removeFile tempName
+  )
+  (
+   \(tempName, tempFile) -> do
+    Data.ByteString.hPutStr tempFile contents
+    System.IO.hClose tempFile
+    System.Directory.renameFile tempName dst
+  )
 
 main :: IO ()
 main = do
  args <- System.Environment.getArgs
  case args of
-  [src, dst] -> putStrLn $ "src = " ++ show src ++ ", dst = " ++ show dst
+  [src, dst] -> copy src dst
   _          -> printUsage
  return ()
 
