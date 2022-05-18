93ca06832f8de590afc32129cfed09d78ae133f7
TaiseiIto <taisei1212@outlook.jp>
2022-05-18 20:20:50 +0900

    9.7

--- 9/7/copy.hs
+++ 9/7/copy.hs
@@ -1,6 +1,17 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import qualified System.Environment
+
 main :: IO ()
 main = do
+ args <- System.Environment.getArgs
+ case args of
+  [src, dst] -> putStrLn $ "src = " ++ show src ++ ", dst = " ++ show dst
+  _          -> printUsage
  return ()
+
+printUsage :: IO ()
+printUsage = do
+ progName <- System.Environment.getProgName
+ putStrLn $ "Usage : ./" ++ progName ++ " <source> <destination>"
 
