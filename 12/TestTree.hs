a82c5f99f3fa28ab535c4490d4a8b2310bfba8f7
TaiseiIto <taisei1212@outlook.jp>
2022-05-23 22:53:24 +0900

    12.4

--- 12/TestTree.hs
+++ 12/TestTree.hs
@@ -1,6 +1,14 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import qualified Data.Foldable
+import qualified Tree
+
 main :: IO ()
-main = do
- return ()
+main = let
+  tree = foldr Tree.treeInsert Tree.EmptyTree $ ([8, 6, 4, 1, 7, 3, 5] :: [Int])
+  treeSum  = Data.Foldable.foldl (+) 0 tree
+  treeMul  = Data.Foldable.foldl (*) 1 tree
+ in do
+  putStrLn $ "treeSum = " ++ show treeSum
+  putStrLn $ "treeMul = " ++ show treeMul
 
