8a65b3652ef0f92d45f021210de270e157863597
TaiseiIto <taisei1212@outlook.jp>
2022-05-31 22:33:09 +0900

    14.8

--- 14/probability.hs
+++ 14/probability.hs
@@ -18,6 +18,21 @@ instance Monad Prob where
  return x = Prob [(x, 1 % 1)]
  m >>= f = flatten . fmap f $ m
 
+data Coin = Heads | Tails deriving (Show, Eq)
+
+coin :: Prob Coin
+coin = Prob [(Heads, 1 % 2), (Tails, 1 % 2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads, 1 % 10), (Tails, 9 % 10)]
+
+flipThree :: Prob Bool
+flipThree = do
+ a <- coin
+ b <- coin
+ c <- loadedCoin
+ return . all (== Tails) $ [a, b, c]
+
 main :: IO ()
-main = putStrLn . (show :: Prob Int -> String) . fmap negate . Prob $ [(3, 1 % 2), (4, 1 % 5), (9, 1 % 4)]
+main = putStrLn . (show :: Prob Bool -> String) $ flipThree
 
