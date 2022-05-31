8312a392041bdccc7c5b2703790194fd994e9d6f
TaiseiIto <taisei1212@outlook.jp>
2022-05-31 21:35:09 +0900

    14.8

--- 14/probability.hs
+++ 14/probability.hs
@@ -2,11 +2,11 @@
 
 import Data.Ratio
 
-newtype Prob a = Prob
- {
-  getProb :: [(a, Rational)]
- } deriving Show
+newtype Prob a = Prob {getProb :: [(a, Rational)]} deriving Show
+
+instance Functor Prob where
+ fmap f (Prob xps) = Prob . map (\(x, p) -> (f x, p)) $ xps
 
 main :: IO ()
-main = putStrLn . (show :: Prob Int -> String) . Prob $ [(3, 1 % 2), (4, 1 % 5), (9, 1 % 4)]
+main = putStrLn . (show :: Prob Int -> String) . fmap negate . Prob $ [(3, 1 % 2), (4, 1 % 5), (9, 1 % 4)]
 
