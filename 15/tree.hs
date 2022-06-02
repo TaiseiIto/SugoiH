038c7d2ed33989cb23a0956607461e49d3664677
TaiseiIto <taisei1212@outlook.jp>
2022-06-02 21:50:00 +0900

    15.1

--- 15/tree.hs
+++ 15/tree.hs
@@ -34,21 +34,25 @@ isRightInTree n
  | n == 0    = False
  | otherwise = not . isLeftInTree $ n
 
-numberElement :: [a] -> [(Int, a)]
-numberElement xs = zip (numbers . length $ xs) xs
+numberList :: [a] -> [(Int, a)]
+numberList xs = zip (numbers . length $ xs) xs
 
 leftList :: [a] -> [a]
-leftList = map snd . filter (isLeftInTree . fst) . numberElement
+leftList = map snd . filter (isLeftInTree . fst) . numberList
 
 rightList :: [a] -> [a]
-rightList = map snd . filter (isRightInTree . fst) . numberElement
+rightList = map snd . filter (isRightInTree . fst) . numberList
 
 list2Tree :: [a] -> Tree a
 list2Tree [] = Empty
 list2Tree (x : xs) = Node x (list2Tree . leftList $ x : xs) (list2Tree . rightList $ x : xs)
 
-freeTree :: Tree Char
-freeTree = list2Tree "POLLYWANTSACRAC"
+numberSubTree :: Int -> Tree a -> Tree (Int, a)
+numberSubTree _ Empty        = Empty
+numberSubTree n (Node x l r) = Node (n, x) (numberSubTree (2 * n + 1) l) (numberSubTree (2 * (n + 1)) r)
+
+numberTree :: Tree a -> Tree (Int, a)
+numberTree = numberSubTree 0
 
 data Direction  = L | R deriving Show
 type Directions = [Direction]
@@ -59,9 +63,12 @@ changeElement (Node x l r) (L : ds) y = Node x (changeElement l ds y) r
 changeElement (Node x l r) (R : ds) y = Node x l (changeElement r ds y)
 changeElement (Node _ l r) [] y       = Node y l r
 
+freeTree :: Tree Char
+freeTree = list2Tree "POLLYWANTSACRAC"
+
 newTree :: Tree Char
 newTree = changeElement freeTree [R, L] 'P'
 
 main :: IO ()
-main = putStrLn . show $ newTree
+main = putStrLn . show . numberTree $ newTree
 
