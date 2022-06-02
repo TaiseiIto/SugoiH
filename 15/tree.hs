90b0ab94580cb1be0c3c199b7078a0ee5800691e
TaiseiIto <taisei1212@outlook.jp>
2022-06-02 20:28:25 +0900

    15.1

--- 15/tree.hs
+++ 15/tree.hs
@@ -1,5 +1,22 @@
 {-# OPTIONS -Wall -Werror #-}
 
+-- data Tree a =
+--  Empty |
+--  Node a (Tree a) (Tree a)
+--  deriving Show
+
+reverseNumbers :: Int -> [Int]
+reverseNumbers n
+ | n < 0     = []
+ | otherwise = (n :) . reverseNumbers $ n - 1
+
+-- leftString :: String -> String
+-- rightString :: String -> String
+
+-- completeBinaryTree :: String -> Tree Char
+-- completeBinaryTree [] = Empty
+-- completeBinaryTree (c : cs) = Node c (completeBinaryTree . leftString $ c : cs) (completeBinaryTree . rightString $ c : cs)
+
 main :: IO ()
-main = return ()
+main = putStrLn . show . reverseNumbers $ 10
 
